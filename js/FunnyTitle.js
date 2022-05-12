df13afb37cd62b7456e72d3bf9d97104870881db
1amfree <2122817150@qq.com>
2022-05-12 19:09:25 +0800

    Site updated: 2022-05-12 19:09:25

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+        // $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
         document.title = '你别走吖 Σ(っ °Д °;)っ';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+        // $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
         document.title = '你终于回来了ヾ(^Д^*)/' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
